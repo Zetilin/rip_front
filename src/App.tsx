6931491f4d5fd5201292dee36c339430dc891e88
Zetilin <yaros1367@yandex.ru>
2025-02-25 20:07:13 +0300

    added tauri

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,22 @@ import { ROUTES } from "../Routes";
 import { HomePage } from "./pages/HomePage";
 import Navigation from "./components/Navigation";
 import './styles.css';
+import { useEffect } from "react";
+
+//const { invoke } = (window as any).__TAURI__.tauri;
 
 function App() {
+  /*useEffect(() => {
+    invoke('tauri', {cmd: 'create'})
+      .then((response: any) => console.log(response))
+      .catch((error: any) => console.log(error));
+    
+      return () => {
+        invoke('tauri', {cmd: 'close'})
+          .then((response: any) => console.log(response))
+          .catch((error: any) => console.log(error));
+      }
+  }, [])*/
   return (
     <BrowserRouter basename="/rip_front">
     <div className="space">
